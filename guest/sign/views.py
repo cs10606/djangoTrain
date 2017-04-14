7cf1b6d5e5b2474c5966b0253aaed26eddaf2cf1
cs10606 <317883861@qq.com>
2017-04-14 08:58:05 +0800

    Initial commit

---
+++ guest/sign/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+# Create your views here.
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+# 登录动作
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+    if username == 'admin' and password == 'admin123':
+        response = HttpResponseRedirect('/event_manage/')
+        response.set_cookie('user', username, 3600)  # 添加浏览器cookie
+        return response
+    else:
+        return render(request,'index.html', {'error': 'username or password error!'})
+
+def event_manage(request):
+    username = request.COOKIES.get('user', '') # 读取浏览器cookie
+    return render(request,"event_manage.html",{"user":username})
